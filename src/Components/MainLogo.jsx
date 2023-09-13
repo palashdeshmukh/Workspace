f007a7378773875f8f826571a21ffae7af9ba135
palashdeshmukh <palash.deshmukh02@gmail.com>
2023-09-13 09:21:34 +0530

    made some changes in the logo

--- src/Components/MainLogo.jsx
+++ src/Components/MainLogo.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { styled } from "styled-components";
 const LogoContainer=styled.div`
-width: 180px;
 padding: 0px 20px;
 margin: 0;
+height:60px;
+line-height:60px;
     
 `
 const LogoText=styled.h2`
 color:#222F3F;
+
 width: 100%;
 text-align: center;
 font-size:45px;
