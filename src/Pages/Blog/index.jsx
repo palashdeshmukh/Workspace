8a551e88b856fda605d62f8a04572f8d3d9c1cf8
palashdeshmukh <palash.deshmukh02@gmail.com>
2023-09-15 00:27:09 +0530

    improved text

--- src/Pages/Blog/index.jsx
+++ src/Pages/Blog/index.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
-import Accordion from "../../Components/Accordion";
-import Cards from "../../Components/Cards";
 const Blog=()=>{
     return(
 <>
